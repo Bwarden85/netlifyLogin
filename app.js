3a1150224b98a2b4fec0549059667b887f2b665f
bwarden <BWarden@proforma.com>
2023-02-28 10:25:23 -0500

    3rd

---
+++ app.js
@@ -0,0 +1,14 @@
+netlifyIdentity.on('init', () => {
+    initUser = netlifyIdentity.currentUser();
+});
+netlifyIdentity.on('login', () => {
+    if (initUser == null) {
+        window.location.replace('landing')
+    }
+    netlifyIdentity.close();
+});
+netlifyIdentity.on('logout', () => {
+    netlifyIdentity.close();
+    window.location.replace('/')
+;
+});
