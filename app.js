fb65c50333d24f9ec8fc937f477ddc4d9e8a7143
bwarden <BWarden@proforma.com>
2023-02-28 11:17:18 -0500

    f

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-netlifyIdentity.on('init', () => {
-    initUser = netlifyIdentity.currentUser();
-});
-netlifyIdentity.on('login', () => {
-    if (initUser == null) {
-        window.location.replace('home')
-    }
-    netlifyIdentity.close();
-});
-netlifyIdentity.on('logout', () => {
-    netlifyIdentity.close();
-    window.location.replace('/')
-        ;
-});
+const app = angular.module('netlifysite', ['ngRoute']);
 
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: 'index.html'
+        })
+        .when("/home", {
+            templateUrl: 'home.html'
+        })
+        .when("/contact", {
+            templateUrl: 'contact.html'
+        });
+
+})
+
+app.controller("netlifyCtrl", function ($scope) {
+
+    netlifyIdentity.on('init', () => {
+        initUser = netlifyIdentity.currentUser();
+    });
+    netlifyIdentity.on('login', () => {
+        if (initUser == null) {
+            window.location.replace('$!home')
+        }
+        netlifyIdentity.close();
+    });
+    netlifyIdentity.on('logout', () => {
+        netlifyIdentity.close();
+        window.location.replace('!$');
+    });
+    
+});
